91382c738690c6e541e92a3e0ecafa33309664cf
mmmhasan-react <2md.mahmudul.hasan@gmail.com>
2025-03-14 09:30:29 +0600

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-unused-expressions": "error",
+      "prepate-constant": true,
+    },
+  },
+  {
+    ignores: ["**/temp.js", "dist/*", "node_modules/*", ".env"],
+  },
+];
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+export default app;
